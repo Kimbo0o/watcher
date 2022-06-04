b704b7b245fbf3480169dda4f055ae38f13ace2d
Kimbo0o <kimkoch1000@gmail.com>
2022-06-04 21:13:15 +0200

    installed pinia module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "@nuxtjs/eslint-module"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@vueuse/nuxt",
+    "@nuxtjs/eslint-module",
+    ["@pinia/nuxt", { disableVuex: true }],
+  ],
 });
