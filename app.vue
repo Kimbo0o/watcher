3930e30d93dbe5ba2d49a1570e248eaccc5111a5
Kimbo0o <kimkoch1000@gmail.com>
2022-06-04 20:58:14 +0200

    added vueuse module

--- app.vue
+++ app.vue
@@ -2,5 +2,6 @@
   <div>
     <!-- <NuxtWelcome /> -->
     <span class="border rounded-full p-4">test</span>
+    <MouseTest></MouseTest>
   </div>
 </template>
---
+++ components/MouseTest.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>pos: {{ x }}, {{ y }}</div>
+</template>
+
+<script setup lang="ts">
+const { x, y } = useMouse();
+</script>
